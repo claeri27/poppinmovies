211cac62dada9d161ae173c65b8e9ea248dfd63f
Christopher Laeri <claeri27@gmail.com>
2021-01-25 21:23:06 -0500

    🐛 added missing dependencies

--- queries/index.ts
+++ queries/index.ts
@@ -1,35 +1,35 @@
-import axios, { AxiosResponse } from 'axios'
+import axios from 'axios'
 import type { ConfigData, LatestData, NowPlayingData, PopularData, TopRatedData } from './types'
 
 const key = process.env.NEXT_PUBLIC_API_KEY
 const baseUrl = `https://api.themoviedb.org/3`
 
-export async function getPopular(page: number) {
-  const popularUrl = `${baseUrl}/movie/popular?api_key=${key}&language=en-US&page=${page}`
-  const res: AxiosResponse<PopularData> = await axios(popularUrl)
+export async function getPopular(page: number): Promise<PopularData> {
+  const url = `${baseUrl}/movie/popular?api_key=${key}&language=en-US&page=${page}`
+  const res = await axios(url)
   return res.data
 }
 
-export async function getTopRated(page: number) {
-  const topRatedUrl = `${baseUrl}/movie/top_rated?api_key=${key}&language=en-US&page=${page}`
-  const res: AxiosResponse<TopRatedData> = await axios(topRatedUrl)
+export async function getTopRated(page: number): Promise<TopRatedData> {
+  const url = `${baseUrl}/movie/top_rated?api_key=${key}&language=en-US&page=${page}`
+  const res = await axios(url)
   return res.data
 }
 
-export async function getNowPlaying(page: number) {
-  const nowPlayingUrl = `${baseUrl}/movie/now_playing?api_key=${key}&language=en-US&page=${page}`
-  const res: AxiosResponse<NowPlayingData> = await axios(nowPlayingUrl)
+export async function getNowPlaying(page: number): Promise<NowPlayingData> {
+  const url = `${baseUrl}/movie/now_playing?api_key=${key}&language=en-US&page=${page}`
+  const res = await axios(url)
   return res.data
 }
 
-export async function getLatest() {
-  const latestUrl = `${baseUrl}/movie/latest?api_key=${key}&language=en-US&page=1`
-  const res: AxiosResponse<LatestData> = await axios(latestUrl)
+export async function getLatest(): Promise<LatestData> {
+  const url = `${baseUrl}/movie/latest?api_key=${key}&language=en-US&page=1`
+  const res = await axios(url)
   return res.data
 }
 
-export async function getConfig() {
-  const configUrl = `${baseUrl}/configuration?api_key=${key}`
-  const res: AxiosResponse<ConfigData> = await axios(configUrl)
+export async function getConfig(): Promise<ConfigData> {
+  const url = `${baseUrl}/configuration?api_key=${key}`
+  const res = await axios(url)
   return res.data
 }
